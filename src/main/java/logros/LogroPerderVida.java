44c77fd729b761de14a2d18f00ebf7c5b15222cc
VictorCanovas <alu0101221953@ull.edu.es>
2024-12-10 16:44:07 +0000

    Clase LogroPerderVida, perder una vida al ser comido

---
+++ src/main/java/logros/LogroPerderVida.java
@@ -0,0 +1,21 @@
+package logros;
+
+import java.util.Objects;
+
+public class LogroPerderVida extends Achievement {
+    public LogroPerderVida() {
+        super("Te comieron", "Perder una vida");
+    }
+
+    @Override
+    public void onEvent(String event, Object data) {
+        if (!this.unlocked && Objects.equals(event, "LOGRO_PERDER_VIDA")) {
+            boolean perderVida = (boolean) data;
+            if (perderVida) {
+                this.unlock();
+                this.showNotification();
+            }
+
+        }
+    }
+}
