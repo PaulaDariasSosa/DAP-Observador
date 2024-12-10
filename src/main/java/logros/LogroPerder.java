9d80cb2f736d80a58e985399dce96a38703467fe
VictorCanovas <alu0101221953@ull.edu.es>
2024-12-10 17:06:22 +0000

    Clase LogroPerder, perder la partida

---
+++ src/main/java/logros/LogroPerder.java
@@ -0,0 +1,21 @@
+package main.java.logros;
+
+import java.util.Objects;
+
+public class LogroPerder extends Achievement {
+    public LogroPerder() {
+        super("Perdedor", "Perder una partida");
+    }
+
+    @Override
+    public void onEvent(String event, Object data) {
+        if (!this.unlocked && Objects.equals(event, "LOGRO_PERDER")) {
+            boolean perder = (boolean) data;
+            if (perder) {
+                this.unlock();
+                this.showNotification();
+            }
+
+        }
+    }
+}
